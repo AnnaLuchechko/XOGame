582fac64428f2a2b5927157888c77eb387e3f2ad
Anna Luchechko <annaluchechko@gmail.com>
2020-11-19 21:51:54 +0200

    Add ComputerState

---
+++ XO-game/State/ComputerState.swift
@@ -0,0 +1,24 @@
+//
+//  ComputerState.swift
+//  XO-game
+//
+//  Created by Anna Luchechko on 19.11.2020.
+//  Copyright © 2020 plasmon. All rights reserved.
+//
+
+import Foundation
+
+class ComputerState: GameState {
+    var isMoveCompleted: Bool = false
+    
+    init() {
+    }
+    
+    func begin() {
+        
+    }
+    
+    func addMark(at position: GameboardPosition) {
+        
+    }
+}
